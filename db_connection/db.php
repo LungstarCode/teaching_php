bccc93c0b75f8abacfc2ebf8485ef1b8c5b1515c
LungstarCode <lungilemawila5@gmail.com>
2026-03-17 08:28:56 +0200

    added a complete connection to the database and fetched data, prinited the fetched data using a lits

--- db_connection/db.php
+++ db_connection/db.php
@@ -1,8 +1,23 @@
 <?php 
 
-    $pdo = new PDO('mysql:host=localhost;dbname=ecommerce_app', 'root', '', [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
+    $pdo = new PDO('mysql:host=localhost;dbname=ecommerce_app', 
+    'root', 
+    '',
+    [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
 
-    var_dump($pdo);
+    $smtp = $pdo->prepare('SELECT * FROM products');
+    $smtp->execute();
+    $results = $smtp->fetchAll(PDO::FETCH_ASSOC);
+   
+    function e($value){
+        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
+    }
+
 ?>
+
+
+    <?php foreach($results AS $result){ ?>
+        <li>
+        <?php echo e($result['name']); ?>
+        </li>
+    <?php } ?>
